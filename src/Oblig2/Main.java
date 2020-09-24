92b85a60511455d0b13afb0d87202b60d6a17a75
Some_Meme <bjorge.andrea@gmail.com>
2020-09-24 10:32:21 +0200

    Måtte gjøre noen endringer i mappestrukturen for å få ting til å fungere. Laget en package til filene som jeg kalte Oblig2. Dersom dette egentlig ikke er lov i følge oppgavebeskrivelsen, så kan vi gjøre endringer på dette før vi leverer.

---
+++ src/Oblig2/Main.java
@@ -0,0 +1,10 @@
+package Oblig2;
+
+import Oblig2.DobbeltLenketListe;
+import Oblig2.Liste;
+
+public class Main {
+    public static void main(String[] args) {
+        Liste<String> liste = new DobbeltLenketListe<>();
+     }
+}
